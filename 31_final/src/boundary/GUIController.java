e9d6c20ad5b520942f049d75c85582a9155e77ef
jonathan <jonathan@LAPTOP-31GUJLK5.eduroam.wireless.dtu.dk>
2017-01-05 14:08:47 +0100

    Ikke noget vigtigt

--- 31_final/src/boundary/GUIController.java
+++ 31_final/src/boundary/GUIController.java
@@ -3,19 +3,55 @@ package boundary;
 import desktop_fields.Field;
 
 import desktop_fields.Street;
+import desktop_fields.Chance;
 import desktop_resources.GUI;
 
 public class GUIController {
 	
 	public void createBoard(){
 		Field[] fields = new Field[40];
-		
 		for (int i = 0; i < 40; i++){
-				fields[i] = new Street.Builder().setTitle(FeltBeskrivelser.fieldNames[i])
-						.setDescription("Leje af grund: " + FeltBeskrivelser.Leje[i][0] + " Kr.")
-						.setSubText("" + FeltBeskrivelser.fieldValue[i])
+			switch(i){
+			case 3:
+			case 8:
+			case 18:
+			case 23:
+			case 34:
+			case 37:
+				
+				fields[i] = new Chance.Builder()
 						.build();
+				break;
+			case 5:
+			case 15:
+				fields[i] = new Chance.Builder()
+						.build();
+				break;
+			case 33:
+			case 39:
+				fields[i] = new Chance.Builder()
+						.build();
+				break;
+			case 10:
+			case 20:
+				fields[i] = new Chance.Builder()
+						.build();
+				break;
+			default:
+				fields[i] = new Chance.Builder()
+						.build();
+				break;
+				
+			}
 		}
+		
+		
+//		for (int i = 0; i < 40; i++){
+//				fields[i] = new Street.Builder().setTitle(FeltBeskrivelser.fieldNames[i])
+//						.setDescription("Leje af grund: " + FeltBeskrivelser.Leje[i][0] + " Kr.")
+//						.setSubText("" + FeltBeskrivelser.fieldValue[i])
+//						.build();
+//		}
 		
 		GUI.create(fields);
 		
