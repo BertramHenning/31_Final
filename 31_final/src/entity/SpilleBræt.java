c5ce18dbc21dfc5745c8984d3c7a1fe9787fd7c8
Bertram <Bertram@Bertram-HP>
2017-01-06 09:43:10 +0100

    start land på felt og spillebræt

--- 31_final/src/entity/SpilleBræt.java
+++ 31_final/src/entity/SpilleBræt.java
@@ -1,5 +1,56 @@
 package entity;
 
+import boundary.FeltBeskrivelser;
+
 public class SpilleBræt {
 
+	Felt[] felter = new Felt[40];
+
+	public SpilleBræt() {
+		for (int i = 0; i < 40; i++) {
+			switch (i) {
+			case 2:
+			case 7:
+			case 17:
+			case 22:
+			case 33:
+			case 36:
+				felter[i] = new PrøvLykken();
+				break;
+			case 4:
+			case 38:
+				felter[i] = new Skat(FeltBeskrivelser.feltVærdi[i]);
+				break;
+			case 5:
+			case 15:
+			case 25:
+			case 35:
+				felter[i] = new Rederi(FeltBeskrivelser.feltVærdi[i]);
+				break;
+			case 12:
+			case 28:
+				felter[i] = new Tapperi(FeltBeskrivelser.feltVærdi[i]);
+				break;
+			case 10:
+			case 30:
+			case 0:
+			case 20:
+				felter[i] = new Fri();
+				break;
+			default:
+				felter[i] = new Grund(FeltBeskrivelser.feltVærdi[i], i / 5 + 1, FeltBeskrivelser.Leje[i]);
+				break;
+
+			}
+		}
+	}
+
+	public Felt[] getFelter() {
+		return felter;
+	}
+
+	public void setFelter(Felt[] felter) {
+		this.felter = felter;
+	}
+	
 }
