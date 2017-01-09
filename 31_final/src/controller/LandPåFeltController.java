3daca6a76a1473dcc0d4e8de5e048ee84152ff36
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-09 13:21:23 +0100

    Landpåfelt

--- 31_final/src/controller/LandPåFeltController.java
+++ 31_final/src/controller/LandPåFeltController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import boundary.GUIController;
+import desktop_resources.GUI;
+import entity.Ejendom;
 import entity.Felt;
 import entity.Spiller;
 
@@ -16,7 +18,7 @@ public class LandPåFeltController {
 
 	public String landPåFelt(Spiller spiller, Felt felt) {
 		if (felt.getClass().getSimpleName().equals("Fri")) {
-			switch(spiller.getPosition()){
+			switch (spiller.getPosition()) {
 			case 0:
 				gui.visBesked("Du landede på Start!");
 				break;
@@ -34,20 +36,41 @@ public class LandPåFeltController {
 				break;
 			}
 		} else if (felt.getClass().getSimpleName().equals("Skat")) {
-			if(spiller.getPosition()==4){
+			if (spiller.getPosition() == 4) {
 				int tiProcent;
-				tiProcent = spiller.getKroner()/10;
-				gui.
+				tiProcent = spiller.getKroner() / 10;
+				if (gui.vælgString("Hvad vil du helst betale?", "4000 kr.", tiProcent + " kr.").equals("4000 kr.")) {
+					spiller.tilføjKroner(-4000);
+				} else {
+					spiller.tilføjKroner(-tiProcent);
+				}
 			} else {
-				
+				gui.visBesked("Du skal betale 2000 kr. i skat");
+				spiller.tilføjKroner(-2000);
 			}
 
 		} else if (felt.getClass().getSimpleName().equals("PrøvLykken")) {
 
 		} else {
+			Ejendom a = (Ejendom) felt;
+			if (a.getEjer() == null && spiller.getKroner() > a.getPris()) {
+				// Lets the player buy the field if there is no owner and the
+				// player has enough coins
+				if (GUI.getUserLeftButtonPressed("Vil du købe " + a.getNavn() + " for " + a.getPris() + " kr.", "Ja", "Nej")) {
+					a.setEjer(spiller);
+					spiller.tilføjKroner(-a.getPris());
+				}
+			} else if (a.getEjer() == null) {
+				GUI.showMessage("Du har ikke nok penge til at købe denne ejendom.");
+			} else if (a.getEjer().equals(spiller)) {
+				GUI.showMessage("Du har landet på din ejen ejedom.");
+			} else {
+				if ()
+
+			}
 
 		}
-		return "test";
+		return "måske ikke string som return alligevel";
 	}
 
 }
