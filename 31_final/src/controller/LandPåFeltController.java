93f1895b16d1ae7731d30e0ed8b57abf507aa3ba
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-09 11:02:58 +0100

    fængsel start og parkering

--- 31_final/src/controller/LandPåFeltController.java
+++ 31_final/src/controller/LandPåFeltController.java
@@ -16,10 +16,33 @@ public class LandPåFeltController {
 
 	public String landPåFelt(Spiller spiller, Felt felt) {
 		if (felt.getClass().getSimpleName().equals("Fri")) {
-			gui.getClass(); // placeholder
+			switch(spiller.getPosition()){
+			case 0:
+				gui.visBesked("Du landede på Start!");
+				break;
+			case 10:
+				gui.visBesked("Du er på besøg i fængslet.");
+				break;
+			case 20:
+				gui.visBesked("Gratis Parkering.");
+				break;
+			case 30:
+				gui.visBesked("Du har lavet ballade, puhaaaaa, i fængsel med dig!!!");
+				spiller.setPosition(10);
+				gui.flytBil(spiller.getNavn(), spiller.getPosition());
+				spiller.setFængsel(1);
+				break;
+			}
 		} else if (felt.getClass().getSimpleName().equals("Skat")) {
+			if(spiller.getPosition()==4){
+				int tiProcent;
+				tiProcent = spiller.getKroner()/10;
+				gui.
+			} else {
+				
+			}
 
-		} else if (felt.getClass().getSimpleName().equals("Skat")) {
+		} else if (felt.getClass().getSimpleName().equals("PrøvLykken")) {
 
 		} else {
 
