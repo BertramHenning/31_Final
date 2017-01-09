378be1828d6dc95dd1cb504744b5167016441197
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-09 10:10:32 +0100

    controllere

--- 31_final/src/controller/LandPåFeltController.java
+++ 31_final/src/controller/LandPåFeltController.java
@@ -5,22 +5,24 @@ import entity.Felt;
 import entity.Spiller;
 
 public class LandPåFeltController {
-	
+
 	private BankController bank;
 	private GUIController gui;
-	//Gui
-	
-	public LandPåFeltController(BankController bank, GUIController gui){
+
+	public LandPåFeltController(BankController bank, GUIController gui) {
 		this.bank = bank;
 		this.gui = gui;
-		//gui
 	}
-	
-	public String landPåFelt(Spiller spiller, Felt felt){
-		if(felt.getClass().getSimpleName().equals("Fri")){
-			
-		}else if(felt.getClass().getSimpleName().equals("Skat")){
-			
+
+	public String landPåFelt(Spiller spiller, Felt felt) {
+		if (felt.getClass().getSimpleName().equals("Fri")) {
+			gui.getClass(); // placeholder
+		} else if (felt.getClass().getSimpleName().equals("Skat")) {
+
+		} else if (felt.getClass().getSimpleName().equals("Skat")) {
+
+		} else {
+
 		}
 		return "test";
 	}
