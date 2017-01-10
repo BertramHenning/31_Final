39324f2d0d4b681329c06a7f5b69ea15599188e4
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-10 16:25:42 +0100

    bank

--- 31_final/src/controller/BankController.java
+++ 31_final/src/controller/BankController.java
@@ -1,5 +1,61 @@
 package controller;
 
+import boundary.GUIController;
+import entity.Felt;
+import entity.Grund;
+import entity.SpilleBræt;
+import entity.Spiller;
+
 public class BankController {
+
+	SpilleBræt bræt;
+	GUIController gui;
+
+	public BankController(GUIController gui) {
+		this.gui = gui;
+		bræt = new SpilleBræt();
+	}
+
+	public void købHus(Spiller spiller) {
+		//////////
+		Grund test1 = (Grund) bræt.getFelt(1);
+		Grund test2 = (Grund) bræt.getFelt(3);
+		test1.setEjer(spiller);
+		test2.setEjer(spiller);
+		///////////////
+		int[] temp = bræt.kanBebygges(spiller);
+		if (temp.length == 0) {
+			gui.visBesked("Du ejer ikke nogen grunde der kan bygges på.");
+		} else {
+			String[] kanBygge = new String[temp.length];
+			for (int i = 0; i < temp.length; i++) {
+				kanBygge[i] = bræt.getNavn(temp[i]);
+			}
+			String valg = gui.vælgString("Hvilken grund vil du bygge et hus på?", kanBygge);
+			int a = 1;
+			for (int i = 0; i < kanBygge.length; i++){
+				if(valg.equals(kanBygge[i])){
+					a = i;
+				}
+			}
+			bræt.tilføjHus(temp[a]);
+		}
+	}
 	
+	public Felt getFelt(int position){
+		return bræt.getFelt(position);
+	}
+	
+	public boolean[] grupperEjet(Spiller spiller){
+		return bræt.grupperEjet(spiller);
+	}
+	
+	public boolean tapperier(Spiller spiller){
+		return bræt.tapperier(spiller);
+	}
+	
+	public int rederier(Spiller spiller){
+		return bræt.rederier(spiller);
+	}
+
 }
