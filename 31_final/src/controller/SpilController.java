62997d38e094fded81b75875c3150ff3c2ef7c70
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-10 16:25:48 +0100

    spil

--- 31_final/src/controller/SpilController.java
+++ 31_final/src/controller/SpilController.java
@@ -1,17 +1,26 @@
 package controller;
 
 import boundary.GUIController;
+import desktop_codebehind.Car;
+import entity.RafleBægre;
 import entity.SpillerListe;
+import entity.Terning;
 
 
 public class SpilController {
 
 	private GUIController gui;
 	private SpillerListe liste;
+	private BankController bank;
+	private LandPåFeltController landPåFelt;
+	private RafleBægre rafle;
 
 
 	public SpilController() {
 		gui = new GUIController();
+		bank = new BankController(gui);
+		landPåFelt = new LandPåFeltController(bank, gui);
+		rafle = new RafleBægre(2);
 
 	}
 
@@ -26,6 +35,53 @@ public class SpilController {
 		for (int i = 0; i < liste.getPlayerAmount(); i++) {
 			System.out.println(i);
 			gui.tilføjSpiller(liste.getNavn(i));
+		}
+		
+		int tur = 0;
+		//Hver tur
+		while(true){
+			if(liste.getPlayerAmount() == 1){
+				gui.visBesked("Du vandt!");
+				break;
+			}
+			
+			if (liste.getFængsel(tur) > 0){
+				//hvad der sker i fængsel
+			} else {
+				gui.visBesked("Det er din tur " + liste.getNavn(tur)+", tryk ok for at rulle terningerne.");
+				
+				rafle.rulTerning();
+				gui.setTerning(rafle.getTerningSlag(0), rafle.getTerningSlag(1));
+				liste.setSum(tur, rafle.getSum());
+				
+				liste.flytPosition(tur, rafle.getSum());
+				gui.flytBil(liste.getNavn(tur), liste.getPosition(tur));
+				
+				landPåFelt.landPåFelt(liste.getSpiller(tur));
+				
+				
+				
+			
+			}
+			
+			for (int i = 0; i < liste.getPlayerAmount(); i++){
+				gui.visPengebeholdning(liste.getNavn(i), liste.getKroner(i));
+			}
+			
+			while(gui.spørgSandtFalsk("Vil du købe et hus?")){
+				bank.købHus(liste.getSpiller(tur));
+			}
+			
+			if(liste.getKroner(tur) <= 0){
+				gui.fjernBil(liste.getNavn(tur));
+				gui.visBesked("du tabte");
+				liste.removePlayer(tur);
+			}
+			
+			tur++;
+			if (tur >= liste.getPlayerAmount()) {
+				tur = 0;
+			}
 		}
 		
 		
@@ -59,6 +115,7 @@ public class SpilController {
 		}
 		liste = new SpillerListe(antalSpillere, navne);
 	}
+	
 	
 	
 
