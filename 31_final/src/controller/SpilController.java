efc01094c3d66149984cdd1283d3e88e76d4fe70
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2017-01-11 12:42:42 +0100

    Spørg kun om huse hvis du kan bygge

--- 31_final/src/controller/SpilController.java
+++ 31_final/src/controller/SpilController.java
@@ -6,7 +6,6 @@ import entity.RafleBægre;
 import entity.SpillerListe;
 import entity.Terning;
 
-
 public class SpilController {
 
 	private GUIController gui;
@@ -14,7 +13,6 @@ public class SpilController {
 	private BankController bank;
 	private LandPåFeltController landPåFelt;
 	private RafleBægre rafle;
-
 
 	public SpilController() {
 		gui = new GUIController();
@@ -28,76 +26,81 @@ public class SpilController {
 
 		gui.createBoard();
 
-		gui.visBesked("Velkommen til Matador! I matador handler det om at købe, udleje eller sælge ejendomme så fordelagtigt, at man bliver den rigeste spiller og eventuelt eneste matador. Man begynder ved Start og flytter bilerne venstre om ifølge terningekast. Når en spillers brik ender på et felt, der ikke allerede ejes af nogen, kan han købe det af banken og indkassere leje af modstanderne, der standser dér. Lejesummen forøges betydeligt ved opførelse af huse og hoteller. For at skaffe flere penge kan man pantsætte grunde til banken. Felterne 'Prøv lykken' giver ret til at trække et kort, hvis ordre derefter må følges.");
-		
+		gui.visBesked(
+				"Velkommen til Matador! I matador handler det om at købe, udleje eller sælge ejendomme så fordelagtigt, at man bliver den rigeste spiller og eventuelt eneste matador. Man begynder ved Start og flytter bilerne venstre om ifølge terningekast. Når en spillers brik ender på et felt, der ikke allerede ejes af nogen, kan han købe det af banken og indkassere leje af modstanderne, der standser dér. Lejesummen forøges betydeligt ved opførelse af huse og hoteller. For at skaffe flere penge kan man pantsætte grunde til banken. Felterne 'Prøv lykken' giver ret til at trække et kort, hvis ordre derefter må følges.");
+
 		tilføjSpiller();
-		//Adds all the players to the gui
+		// Adds all the players to the gui
 		for (int i = 0; i < liste.getPlayerAmount(); i++) {
 			System.out.println(i);
 			gui.tilføjSpiller(liste.getNavn(i));
 		}
-		
+
 		int tur = 0;
-		//Hver tur
-		while(true){
-			if(liste.getPlayerAmount() == 1){
+		// Hver tur
+		while (true) {
+			if (liste.getPlayerAmount() == 1) {
 				gui.visBesked("Du vandt!");
 				break;
 			}
-			
-			if (liste.getFængsel(tur) > 0){
-				if(gui.spørgSandtFalsk("Vil du betale 1000 kr. for at komme ud af fængslet?")){
+
+			if (liste.getFængsel(tur) > 0) {
+				String[] valg = { "Betal 1000 kr.", "Prøv at slå 2 ens." };
+				String valgt = gui.vælgString("Vil du betale 1000 kr. for at komme ud af fængslet?", valg);
+				if(valgt.equals("Betal 1000 kr.")){
 					liste.tilføjKroner(tur, -1000);
 					liste.setFængsel(tur, 0);
 					gui.visPengebeholdning(liste.getNavn(tur), liste.getKroner(tur));
+				} else{
+					rafle.rulTerning();
+					gui.setTerning(rafle.getTerningSlag(0), rafle.getTerningSlag(1));
+					if (rafle.getTerningSlag(0)==rafle.getTerningSlag(1)){
+						liste.setFængsel(tur, 0);
+					}
 				}
-			} 
-			if(liste.getFængsel(tur) == 0){
-				gui.visBesked("Det er din tur " + liste.getNavn(tur)+", tryk ok for at rulle terningerne.");
-				
+
+			}
+			if (liste.getFængsel(tur) == 0) {
+				gui.visBesked("Det er din tur " + liste.getNavn(tur) + ", tryk ok for at rulle terningerne.");
+
 				rafle.rulTerning();
 				gui.setTerning(rafle.getTerningSlag(0), rafle.getTerningSlag(1));
 				liste.setSum(tur, rafle.getSum());
-				
+
 				liste.flytPosition(tur, rafle.getSum());
 				gui.flytBil(liste.getNavn(tur), liste.getPosition(tur));
-				
+
 				gui.visPengebeholdning(liste.getNavn(tur), liste.getKroner(tur));
-				
+
 				landPåFelt.landPåFelt(liste.getSpiller(tur));
-				
-				
-				
-			
+
 			}
-			
-			for (int i = 0; i < liste.getPlayerAmount(); i++){
+
+			for (int i = 0; i < liste.getPlayerAmount(); i++) {
 				gui.visPengebeholdning(liste.getNavn(i), liste.getKroner(i));
 			}
 			
-			while(gui.spørgSandtFalsk("Vil du købe et hus?")){
+			if(bank.kanKøbeHus(liste.getSpiller(tur))){
+			while (gui.spørgSandtFalsk("Vil du købe et hus?")) {
 				bank.købHus(liste.getSpiller(tur));
 				gui.visPengebeholdning(liste.getNavn(tur), liste.getKroner(tur));
 			}
-			
-			if(liste.getKroner(tur) <= 0){
+			}
+
+			if (liste.getKroner(tur) <= 0) {
 				gui.fjernBil(liste.getNavn(tur));
 				gui.visBesked("du tabte");
 				liste.removePlayer(tur);
 			}
-			
+
 			tur++;
 			if (tur >= liste.getPlayerAmount()) {
 				tur = 0;
 			}
 		}
-		
-		
-
-		
 
 	}
-	
+
 	/**
 	 * Tilføjer spillere til spillerlisten ud fra bruger input
 	 */
@@ -106,25 +109,22 @@ public class SpilController {
 		int antalSpillere = gui.spørgBeløb("Indtast antal spillere: ", 3, 6);
 		String[] navne = new String[antalSpillere];
 		for (int i = 0; i < antalSpillere; i++) {
-			//Tests if the new name is the same as another players name
-			while(true){
+			// Tests if the new name is the same as another players name
+			while (true) {
 				boolean sammeNavn = false;
 				navne[i] = gui.spørgString("Spiller " + (i + 1) + " indtast dit navn:");
-				for (int j = 0; j < i; j++){
-					if (navne[i].equals(navne[j])){
+				for (int j = 0; j < i; j++) {
+					if (navne[i].equals(navne[j])) {
 						sammeNavn = true;
 						gui.visBesked("Du kan ikke have det samme navn.");
 					}
 				}
-				if (sammeNavn == false){
+				if (sammeNavn == false) {
 					break;
 				}
 			}
 		}
 		liste = new SpillerListe(antalSpillere, navne);
 	}
-	
-	
-	
 
 }
