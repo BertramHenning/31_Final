97f17400e7b9f617406df9f41f3607363260b9dd
Thomas <Thomas@Tklorentzen-PC.home>
2017-01-12 18:05:47 +0100

    tilføjet kommentarer

--- 31_final/src/entity/PrøvLykken.java
+++ 31_final/src/entity/PrøvLykken.java
@@ -1,5 +1,10 @@
 package entity;
 
+/**
+ * 
+ * @author Thomas
+ *PrøvLykken er en abstract superklasse, som de andre lykkekort nedarver navnevariablen fra.
+ */
 public class PrøvLykken extends Felt {
 
 	public PrøvLykken(String navn) {
